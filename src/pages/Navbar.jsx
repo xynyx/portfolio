8a36a3ab2249350852aa1356c0619bc25af12a74
xynyx <matt.taylr22@gmail.com>
2020-04-26 18:20:48 -0400

    Move nav elems to right; customize them

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -10,14 +10,15 @@ import {
 
 export default function Navigation() {
   return (
-    <Navbar className="transparent" bg="transparent" expand="lg">
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar variant="dark" className="navbar" bg="transparent" expand="lg">
+      <Navbar.Brand href="https://github.com/xynyx">XYNYX</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">About</Nav.Link>
+          <Nav.Link href="#link">pRojects</Nav.Link>
+          <Nav.Link href="#link">contAct</Nav.Link>
+          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
               Another action
@@ -27,12 +28,12 @@ export default function Navigation() {
             <NavDropdown.Item href="#action/3.4">
               Separated link
             </NavDropdown.Item>
-          </NavDropdown>
+          </NavDropdown> */}
         </Nav>
-        <Form inline>
+        {/* <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
-        </Form>
+        </Form> */}
       </Navbar.Collapse>
     </Navbar>
   )

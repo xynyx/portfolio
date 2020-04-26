0d5b8c165f785205ff4481b6d1b6224a45b18f19
xynyx <matt.taylr22@gmail.com>
2020-04-26 16:25:12 -0400

    Add parallax background; name; fonts

---
+++ src/pages/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="name">
+      <div className="glitch text" data-text="Matthew Taylor">mAtthew tAylor
+      <h2>Full-Stack Developer</h2>
+      </div>
+    </header>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
 import React from "react"
+import Header from "./Header"
+import "../styles/header.scss"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  return (
+    <>
+      <link href="https://fonts.googleapis.com/css?family=Major+Mono+Display|Montserrat&display=swap" rel="stylesheet"/>
+      <div className="container"></div>
+      <Header />
+    </>
+  )
+}

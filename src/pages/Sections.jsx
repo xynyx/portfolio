b1f73d9fab7e94f0d8f6ecfcf8612922026422fe
xynyx <matt.taylr22@gmail.com>
2020-06-23 16:26:07 -0400

    Add projects & contact section

--- src/pages/Sections.jsx
+++ src/pages/Sections.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import About from "./About";
+import React from "react"
+import About from "./About"
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 export default function Sections() {
   return (
     <div className="container-fluid">
       {/* <div className="container-inner"></div> */}
       <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
-
   )
 }

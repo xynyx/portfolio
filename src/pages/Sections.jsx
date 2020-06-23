996666b64519dc60a837b0cac89de3f736098205
xynyx <matt.taylr22@gmail.com>
2020-06-23 11:52:02 -0400

    Add about section

--- src/pages/Sections.jsx
+++ src/pages/Sections.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import About from "./About";
 
 export default function Sections() {
   return (
     <div className="container-fluid">
-      <div className="container-inner"></div>
+      {/* <div className="container-inner"></div> */}
+      <About />
     </div>
 
   )

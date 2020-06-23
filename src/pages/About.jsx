95a0bbbae5d7e7444c200cb508d1ace33c47a772
xynyx <matt.taylr22@gmail.com>
2020-06-23 12:53:18 -0400

    about section in place

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <div className="section--about">
       <p>About</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia laoreet est et consequat. Aliquam erat volutpat. Etiam velit erat, placerat tincidunt molestie a, tempor et eros. Nam tempus eros ut orci malesuada, vitae tincidunt neque egestas. Donec id luctus sapien. Duis et metus ut ligula imperdiet sagittis. Donec tempus finibus nisi, in hendrerit erat lobortis ullamcorper. Integer venenatis, sapien vel dignissim fringilla, orci velit consectetur augue, id mollis metus ante vel nunc. In eu sapien sem. Curabitur condimentum, purus gravida volutpat rutrum, erat augue luctus est, a sodales sem libero non libero.</p>
     </div>
   )
 }

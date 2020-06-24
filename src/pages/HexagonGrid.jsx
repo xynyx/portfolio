76d6aecb965b0c7cc8ef505301d47eb97dbe82ad
xynyx <matt.taylr22@gmail.com>
2020-06-24 13:35:30 -0400

    Replace generic hex with image hex

--- src/pages/HexagonGrid.jsx
+++ src/pages/HexagonGrid.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React from "react"
 
 export default function HexagonGrid() {
   return (
     <div className="hexagon-container">
-      <div className="hexagon"></div>
-      <div className="hexagon"></div>
-      <div className="hexagon"></div>
-      <div className="hexagon"></div>
-      <div className="hexagon"></div>
-      <div className="hexagon"></div>
+      <div className="hexagon">
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
+      <div className="hexagon">
+        {" "}
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
+      <div className="hexagon">
+        {" "}
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
+      <div className="hexagon">
+        {" "}
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
+      <div className="hexagon">
+        {" "}
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
+      <div className="hexagon">
+        {" "}
+        <div class="hexTop"></div>
+        <div class="hexBottom"></div>
+      </div>
     </div>
   )
 }

8ba64db882ac68c1fb19d60b50eb817bd37c9dae
xynyx <matt.taylr22@gmail.com>
2020-06-24 10:58:50 -0400

    Begin hexagon project grid

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react"
 
 export default function Contact() {
   return (
     <div>
-          <div className="section--projects">
-      <p>CONTACT</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia laoreet est et consequat. Aliquam erat volutpat. Etiam velit erat, placerat tincidunt molestie a, tempor et eros. Nam tempus eros ut orci malesuada, vitae tincidunt neque egestas. Donec id luctus sapien. Duis et metus ut ligula imperdiet sagittis. Donec tempus finibus nisi, in hendrerit erat lobortis ullamcorper. Integer venenatis, sapien vel dignissim fringilla, orci velit consectetur augue, id mollis metus ante vel nunc. In eu sapien sem. Curabitur condimentum, purus gravida volutpat rutrum, erat augue luctus est, a sodales sem libero non libero.</p>
-    </div>
+      <div className="section--projects">
+        <p>contAct</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          lacinia laoreet est et consequat. Aliquam erat volutpat. Etiam velit
+          erat, placerat tincidunt molestie a, tempor et eros. Nam tempus eros
+          ut orci malesuada, vitae tincidunt neque egestas. Donec id luctus
+          sapien. Duis et metus ut ligula imperdiet sagittis. Donec tempus
+          finibus nisi, in hendrerit erat lobortis ullamcorper. Integer
+          venenatis, sapien vel dignissim fringilla, orci velit consectetur
+          augue, id mollis metus ante vel nunc. In eu sapien sem. Curabitur
+          condimentum, purus gravida volutpat rutrum, erat augue luctus est, a
+          sodales sem libero non libero.
+        </p>
+      </div>
     </div>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react"
+import HexagonGrid from "./HexagonGrid"
 
 export default function Projects() {
   return (
     <div className="section--projects">
-      <p>projects</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia laoreet est et consequat. Aliquam erat volutpat. Etiam velit erat, placerat tincidunt molestie a, tempor et eros. Nam tempus eros ut orci malesuada, vitae tincidunt neque egestas. Donec id luctus sapien. Duis et metus ut ligula imperdiet sagittis. Donec tempus finibus nisi, in hendrerit erat lobortis ullamcorper. Integer venenatis, sapien vel dignissim fringilla, orci velit consectetur augue, id mollis metus ante vel nunc. In eu sapien sem. Curabitur condimentum, purus gravida volutpat rutrum, erat augue luctus est, a sodales sem libero non libero.</p>
+      <p>pRojects</p>
+      <HexagonGrid />
     </div>
   )
 }

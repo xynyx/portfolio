2a3bf2aa82598aa3e2da93a4a70a77ee09a63bef
xynyx <matt.taylr22@gmail.com>
2020-06-24 11:28:30 -0400

    Hexagon grid in place

--- src/pages/HexagonGrid.jsx
+++ src/pages/HexagonGrid.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 export default function HexagonGrid() {
   return (
-    <div>
+    <div className="hexagon-container">
+      <div className="hexagon"></div>
+      <div className="hexagon"></div>
+      <div className="hexagon"></div>
+      <div className="hexagon"></div>
+      <div className="hexagon"></div>
       <div className="hexagon"></div>
     </div>
   )

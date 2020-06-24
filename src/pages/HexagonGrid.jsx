2bc8080d79e81ee2a7234ea7d78188b186ff526c
xynyx <matt.taylr22@gmail.com>
2020-06-24 15:42:19 -0400

    Add screenshots to hexes of projects

--- src/pages/HexagonGrid.jsx
+++ src/pages/HexagonGrid.jsx
@@ -3,31 +3,31 @@ import React from "react"
 export default function HexagonGrid() {
   return (
     <div className="hexagon-container">
-      <div className="hexagon">
+      <div className="hexagon" id="focuspocus">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="lockr">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="color-picker">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="mtg">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="forex">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="interview-scheduler">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>
-      <div className="hexagon">
+      <div className="hexagon" id="tinyapp">
         <div className="hexTop"></div>
         <div className="hexBottom"></div>
       </div>

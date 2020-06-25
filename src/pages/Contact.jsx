b9bccb59eb04854edf3909870f6aa172383bab12
xynyx <matt.taylr22@gmail.com>
2020-06-25 13:48:14 -0400

    install ant design UI; set up basic form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,73 @@
 import React from "react"
+import { Form, Input, InputNumber, Button } from "antd";
+import 'antd/dist/antd.css'
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+}
+const validateMessages = {
+  required: "${label} is required!",
+  types: {
+    email: "${label} is not validate email!",
+    number: "${label} is not a validate number!",
+  },
+  number: {
+    range: "${label} must be between ${min} and ${max}",
+  },
+}
 
 export default function Contact() {
+  const onFinish = values => {
+    console.log(values)
+  }
   return (
     <div>
       <div className="section--projects">
         <p>contAct</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          lacinia laoreet est et consequat. Aliquam erat volutpat. Etiam velit
-          erat, placerat tincidunt molestie a, tempor et eros. Nam tempus eros
-          ut orci malesuada, vitae tincidunt neque egestas. Donec id luctus
-          sapien. Duis et metus ut ligula imperdiet sagittis. Donec tempus
-          finibus nisi, in hendrerit erat lobortis ullamcorper. Integer
-          venenatis, sapien vel dignissim fringilla, orci velit consectetur
-          augue, id mollis metus ante vel nunc. In eu sapien sem. Curabitur
-          condimentum, purus gravida volutpat rutrum, erat augue luctus est, a
-          sodales sem libero non libero.
-        </p>
+
+        <Form
+          {...layout}
+          name="nest-messages"
+          onFinish={onFinish}
+          validateMessages={validateMessages}
+        >
+          <Form.Item
+            name={["user", "name"]}
+            label="Name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={["user", "email"]}
+            label="Email"
+            rules={[
+              {
+                type: "email",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item name={["user", "introduction"]} label="Introduction">
+            <Input.TextArea />
+          </Form.Item>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   )

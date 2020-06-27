4773e704fb6555ebae5345b21be91c9226b537ef
xynyx <matt.taylr22@gmail.com>
2020-06-27 15:22:26 -0400

    exclude node_modules in serverless config

--- handler.js
+++ handler.js
@@ -1,18 +1,20 @@
-'use strict';
+"use strict"
 
 module.exports.mailer = async (event, context, callback) => {
-  return {
+  const response = {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: "Go Serverless v1.0! Your function executed successfully!",
         input: event,
       },
       null,
       2
     ),
-  };
+  }
+
+  callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+}

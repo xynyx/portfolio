13e61584b740f3754ba989171d0b6e161dd2bd9b
xynyx <matt.taylr22@gmail.com>
2020-06-27 16:26:01 -0400

    Emailer works (received in inbox!)

--- handler.js
+++ handler.js
@@ -5,7 +5,27 @@ const SES = new AWS.SES()
 
 function sendEmail(formData, cb) {
   // Build the SES parameters
+  const emailParams = {
+    Source: "matt.taylr22@gmail.com",
+    ReplyToAddresses: [formData.email],
+    Destination: {
+      ToAddresses: ["matt.taylr22@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.email}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "New message!"
+      }
+    },
+  }
   // Send the email
+  SES.sendEmail(emailParams, cb);
 }
 
 module.exports.mailer = async (event, context, callback) => {
@@ -18,8 +38,6 @@ module.exports.mailer = async (event, context, callback) => {
       console.log(data)
     }
   })
-
-  callback(null, response)
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };

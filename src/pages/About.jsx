b35716928d28947ad6e6d44988b5ff581a40e5f1
xynyx <matt.taylr22@gmail.com>
2020-07-02 21:50:30 -0400

    SPlit about section into 3; add svg icons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,19 +5,36 @@ export default function About() {
     <div className="section--about">
       <p>About</p>
       <p>
-      {/* Hi!
+        <div className="about-container">
+          <div className="about-section">
+            {/* <img src="/028-coding.svg"></img> */}
+            <img src="/robot.svg"></img>
+            Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast
+            yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin
+            grog yardarm hempen halter furl.
+          </div>
+          <div className="about-section">
+            <img src="/ai.svg"></img>
+            Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast
+            yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin
+            grog yardarm hempen halter furl.
+          </div>
+          <div className="about-section">
+            <img src="/vr.svg"></img>
+            Prow scuttle parrel provost Sail ho shrouds spirits boom mizzenmast
+            yardarm. Pinnace holystone mizzenmast quarter crow's nest nipperkin
+            grog yardarm hempen halter furl.
+          </div>
+        </div>
 
-      I'm a Toronto-based full stack developer with a passion for creating beautiful and functional user experiences, and a knack for writing clean, elegant, and efficient code.
+        {/*
+      
 
-      As a former laboratory scientist in microbiology, professional flautist, and with a love for painting and modeling, I've got some pretty nifty, unique skills you might not find elsewhere!
+      I'm a Toronto-based full stack developer with a zest for creating beautiful and functional user experiences, and a knack for writing clean, elegant, and efficient code.
+
+      As a former laboratory scientist in microbiology, professional flautist, and with a love for painting, I've got some pretty nifty, unique skills you might not find elsewhere!
 
       Obsessed with all things Sci-Fi. Virtual Reality enthusiast. Paints miniature models. Outdoorsy and adventurous. */}
-
-
-
-
-      Goofy?
-
       </p>
     </div>
   )
